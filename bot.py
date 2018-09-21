f65bafa5cffe19bc89c2a7a6cfcfab007e5bf9fd
raluvy95 <43247023+raluvy95@users.noreply.github.com>
2018-09-21 19:37:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -127,6 +127,13 @@ async def shrug(ctx):
 async def hug(ctx):
     await ctx.send("(つ ͡° ͜ʖ ͡°)つ")
 
+@bot.command(name='8ball')
+async def lball(ctx, question = None):
+	if question is None:
+		return await ctx.send('<:RaluvyQuestion:489805105764499467> | **Please put a question!**')
+	if question is not None:
+		await ctx.send(random.choice(['● It is certain.', '● It is decidedly so.', '● Without a doubt.', '● Yes - definitely.', '● You may rely on it', '● As I see it, yes.', '● Most likely.', '● Outlook good.', '● Yes.', '● Signs point to yes.', '● Reply hazy, try again', '● Ask again later.', '● Better not tell you now.', '● Cannot predict now.', '● Concentrate and ask again.', '● Don`t count on it.', '● My reply is no.', '● My sources say no', '● Outlook not so good.', '● Very doubtful.' ]))
+		
 @bot.command()
 async def kys(ctx):
     await ctx.send("Nu te sinucide :)")
