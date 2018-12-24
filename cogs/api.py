ea930beb51d8075048f1f86fa3592f82bc5c214c
raluvy95 <43247023+raluvy95@users.noreply.github.com>
2018-12-24 14:10:28 +0200

    Update api.py

--- cogs/api.py
+++ cogs/api.py
@@ -63,6 +63,49 @@ class API():
 				embed.timestamp = datetime.datetime.utcnow()
 				await ctx.send(embed=embed)
 
+	
+	@commands.command()
+	@commands.cooldown(1, 5, commands.BucketType.user)
+	async def meme(self, ctx):
+		async with aiohttp.ClientSession() as cs:
+			async with cs.get("https://api-to.get-a.life/meme") as r:
+				res = await r.json()
+				embed = discord.Embed(color=discord.Colour.red())
+				embed.title = 'Random meme!'
+				embed.set_image(url=res['url'])
+				embed.set_footer(text=f"{self.bot.user.name}")
+				embed.timestamp = datetime.datetime.utcnow()
+				await ctx.send(embed=embed)
+				
+	@commands.command()
+	@commands.cooldown(1, 5, commands.BucketType.user)
+	async def pikachu(self, ctx):
+		async with aiohttp.ClientSession() as cs:
+			async with cs.get("https://api-to.get-a.life/pikachuimg") as r:
+				res = await r.json()
+				embed = discord.Embed(color=discord.Colour.red())
+				embed.title = 'Pika!'
+				embed.set_image(url=res['link'])
+				embed.set_footer(text=f"{self.bot.user.name}")
+				embed.timestamp = datetime.datetime.utcnow()
+				await ctx.send(embed=embed)
+				
+	@commands.command(aliases=['catfact'])
+	@commands.cooldown(1, 5, commands.BucketType.user)
+	async def catfacts(self, ctx):
+		async with aiohttp.ClientSession() as cs:
+			async with cs.get("https://api-to.get-a.life/catfact") as r:
+				res = await r.json()
+				await ctx.send(f":cat: **{res['fact']}**")
+					       
+	@commands.command(aliases=['dogfact'])
+	@commands.cooldown(1, 5, commands.BucketType.user)
+	async def dogfacts(self, ctx):
+		async with aiohttp.ClientSession() as cs:
+			async with cs.get("https://api-to.get-a.life/dogfact") as r:
+				res = await r.json()
+				await ctx.send(f":dog: **{res['fact']}**")
+
 
 
 
